75393720d9c9dd590d45b3e72659564054ea7fa8
Mohammad <othman.othman2004v@gmail.com>
2026-03-08 01:59:46 +0300

    add task 4

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,21 @@
-
+// Task 2
 function functionToExrcute(){
     console.log("Hello Word");
+}
+
+// Task 4
+function createArrayAndSum(){
+
+    let numbers = [];
+    let sum = 0;
+
+    for(let i = 0; i <= 100; i++){
+        numbers.push(i);
+    }
+
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+
+    console.log("Sum =", sum);
 }
