967dce3522bbec2c53a33e9a8b7cc88d7b65e205
Mohammad <othman.othman2004v@gmail.com>
2026-03-08 02:08:03 +0300

    add task 5

--- task2/index.js
+++ task2/index.js
@@ -19,3 +19,22 @@ function createArrayAndSum(){
 
     console.log("Sum =", sum);
 }
+
+// Task 5 
+function createArrayAndSumEven(){
+
+    let numbers = [];
+    let sum = 0;
+
+    for(let i = 0; i <= 100; i++){
+        numbers.push(i);
+    }
+
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] % 2 === 0 ){
+            sum+= numbers[i]
+        }
+    }
+
+    console.log("Sum =", sum);
+}
