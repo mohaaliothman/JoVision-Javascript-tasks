ddde7a18013ea6301d0e5072564c41faa99d0d86
Mohammad <othman.othman2004v@gmail.com>
2026-03-11 17:25:06 +0300

    Task 6

--- task2/index.js
+++ task2/index.js
@@ -38,3 +38,70 @@ function createArrayAndSumEven(){
 
     console.log("Sum =", sum);
 }
+
+//Task 6
+
+function createArray(){
+    let arr =[];
+    for (let i =0 ;i <= 100 ; i++){
+        arr.push(i);
+    }
+    return arr;
+}
+
+function shuffleArray(array){
+    for(let i = array.length -1; i>0 ; i--){
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i] ,array[j]] = [array[j] ,array[i]];
+    }
+}
+
+function buttonOne(){
+    let arr = createArray() ;
+
+    let result = arr.filter(function(num){
+        return num % 3 !== 0 ;
+    });
+
+    console.log(result);
+}
+
+function buttonTwo(){
+    let arr = createArray();
+
+    for (let i = 100 ; i<= 150 ; i++){
+        arr.push(i);
+    }
+    console.log(arr)
+}
+
+function buttonThree(){
+    let arr = createArray();
+
+    let result = arr.map(function(num){
+        return num + 3;
+    });
+    console.log(result);
+}
+
+function buttonFour() {
+    let arr = createArray();
+
+    let result = arr.slice(20, 41);
+
+    console.log(result);
+}
+
+function buttonFive() {
+    let arr = createArray();
+
+    shuffleArray(arr);
+
+    console.log("Shuffled:", arr);
+
+    arr.sort(function(a, b) {
+        return b - a;
+    });
+
+    console.log("Descending:", arr);
+}
